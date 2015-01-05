d84e0db1c4c471af44505675ea7b34cc3d06e532
Jay Renteria <jrenteria13@cnm.edu>
2015-01-05 13:26:59 -0700

    Started the prework, activity of chapter 5 in PHP book.

---
+++ Chapter 5 PHP Activity 2.php
@@ -0,0 +1,27 @@
+<?php
+$thor = 48;
+$loki = 47;
+if ($thor == $loki) {
+	echo "true";
+} else {
+	echo "false";
+};
+echo "<br/>";
+if ($thor < $loki) {
+	echo "true";
+} else {
+	echo "false";
+};
+echo "<br/>";
+if ($thor > $loki) {
+	echo "true";
+} else {
+	echo "false";
+};
+echo "<br/>";
+if ($thor <= $loki) {
+	echo "true";
+} else {
+	echo "false";
+};
+?>
